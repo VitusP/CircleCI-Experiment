43bc997e542941f298bf483922acb8118bb88565
vaputra <vaputra@ncsu.edu>
2023-05-07 08:53:08 -0400

    Set production server and update site

--- src/controller.py
+++ src/controller.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 import markdown
 import os
 
-
-app = Flask(__name__) # template_folder=os.path.join(os.getcwd(), 'src/templates/'
+isDebug = os.environ.get('DEBUG', False)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -13,4 +13,4 @@ def index():
     return render_template('index.html', content=md)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=isDebug, host="0.0.0.0", port=8080)
